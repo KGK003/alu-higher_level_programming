ca70c694fab100d6bff7cc68833ef81003ffa2df
Keane <g.keane@alustudent.com>
2024-11-09 00:22:25 -0800

    initial changes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ the specified class (directly or indirectly).
 
 def inherits_from(obj, a_class):
     """
-    Returns True if the object is an instance of a class that inherited from 
+    Returns True if the object is an instance of a class that inherited from
     the specified class (directly or indirectly); otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
