7b3ba43fb8eba7b4be31702412f8067f3215d341
Keane <g.keane@alustudent.com>
2024-11-12 12:14:43 -0800

    task

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+d file function"""
+
+
+def read_file(filename=""):
+        """reads a file and prints it out"""
+            with open(filename, encoding="utf-8") as f:
+                    print(f.read(), end="")
