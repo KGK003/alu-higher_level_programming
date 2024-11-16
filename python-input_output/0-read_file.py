84c4a756af463faea1d95d1c88cf47727f0e6567
Keane <g.keane@alustudent.com>
2024-11-16 04:13:18 -0800

    changes

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
+""" Function that reads a text file (UTF8) and prints it to stdout """
+
 
 def read_file(filename=""):
-    """
-    Reads the contents of a UTF-8 encoded text file and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
-
-    If the file does not exist or is empty, no output is printed.
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            print(file.read(), end="")
-    except FileNotFoundError:
-        pass  # If the file doesn't exist, do nothing
-    except IOError:
-        pass  # Handles other I/O errors (e.g., permission issues), although not required to handle in this case
+    """ Reads UFT-8 File """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
