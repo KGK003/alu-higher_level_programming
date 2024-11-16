7441b89849b9d33ee0f138675a61cc00fd6b40dc
Keane <g.keane@alustudent.com>
2024-11-16 04:15:21 -0800

    changes

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
+""" Write to a file """
 
-def read_file(filename=""):
-    """
-    Reads the contents of a UTF-8 encoded text file and prints it to stdout.
 
-    Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
-
-    If the file does not exist or is empty, no output is printed.
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            print(file.read(), end="")
-    except FileNotFoundError:
-        pass  # If the file doesn't exist, do nothing
-    except IOError:
-        pass  # Handles other I/O errors (e.g., permission issues), although not required to handle in this case
-
+def write_file(filename="", text=""):
+    """ Writes a string to a text file (UTF8) and returns
+    the number of characters written """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (f.write(text))
