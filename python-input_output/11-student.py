fdd7b31df407abedd3e05ad0c56199258c47328e
Keane <g.keane@alustudent.com>
2024-11-16 06:19:51 -0800

    change

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -6,17 +6,20 @@ class Student:
     """ New class student """
 
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Retrieves a dictionary representation of a Student instance """
+        """Returns a dictionary representation of the Student instance."""
         if attrs is None:
-            return (self.__dict__)
+            return self.__dict__  # Return all attributes as a dictionary
         else:
-            dic = {}
-            for nm in attrs:
-                if hasattr(self, nm):
-                    dic[nm] = getattr(self, nm)
-            return (dic)
+            # Return only the attributes in the attrs list
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance with values from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
