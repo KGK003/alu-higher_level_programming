ae1bbc9de1d3eae09ba22c441ff8a95ca3fd605b
Keane <g.keane@alustudent.com>
2024-11-22 06:04:36 -0800

    changed

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request with a custom header and displays the body of the response.
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+response=$(curl -s -H "X-HolbertonSchool-User-Id: 98" "$1") [ "$response" == "NOP" ] && echo "OK"
