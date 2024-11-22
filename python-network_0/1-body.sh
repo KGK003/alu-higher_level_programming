f2c23d30c6659689182441b8538cf0a7ba5d03b6
Keane <g.keane@alustudent.com>
2024-11-22 05:39:02 -0800

    change

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# This script takes a URL, sends a request to it, and displays the size of the response body in bytes.
-curl -s "$1" | wc -c
+# Sends a GET request and displays body if 200 status code, else shows redirection count.
+response=$(curl -s -L -w "%{http_code}" -o /tmp/response.txt "$1")
+[ "$response" -eq 200 ] && cat /tmp/response.txt || echo "Redirection count: $(grep -c 'HTTP/1.1 3' /tmp/response.txt)"
