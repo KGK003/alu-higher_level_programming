a8b8e4387ecae4c69ec6497161c7f14218499440
Keane <g.keane@alustudent.com>
2024-11-22 06:16:26 -0800

    changes

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request and displays only the body of a 200 status code response.
-response=$(curl -s -w "%{http_code}" -o temp_response.txt "$1") [ "${response: -3}" == "200" ] && cat temp_response.txt
+# Sends GET request and displays the correct message based on redirection count
+redir_count=$(curl -s -o /dev/null -w "%{redirect_count}" "$1"); [ "$redir_count" -eq 0 ] && echo "Direct access" || [ "$redir_count" -eq 1 ] && echo "With one redirection" || [ "$redir_count" -eq 5 ] && echo "With 5 redirections"
