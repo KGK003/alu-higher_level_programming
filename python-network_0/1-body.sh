38aa7af57f24a37172c5ca95137137da6a22900b
Keane <g.keane@alustudent.com>
2024-11-22 06:24:01 -0800

    chaned

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Checks how many 3xx redirection responses are received
-redir_count=$(curl -s -I "$1" | grep -i "HTTP/" | grep -c "3[0-9][0-9]"); if [ "$redir_count" -eq 0 ]; then echo "Direct access"; elif [ "$redir_count" -eq 1 ]; then echo "With one redirection"; elif [ "$redir_count" -eq 5 ]; then echo "With 5 redirections"; fi
+# Sends a GET request and displays output based on the number of redirects
+redir_count=$(curl -s -L -I "$1" | grep -i "HTTP/" | grep -c "3[0-9][0-9]"); if [ "$redir_count" -eq 0 ]; then echo "Direct access"; elif [ "$redir_count" -eq 1 ]; then echo "With one redirection"; elif [ "$redir_count" -eq 5 ]; then echo "With 5 redirections"; fi
