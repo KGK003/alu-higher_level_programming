cd94a4b9e4dbefabe47da547eac4a00ecc2510b0
Keane <g.keane@alustudent.com>
2024-11-22 06:27:31 -0800

    initial change

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request and displays output based on the number of redirects
-redir_count=$(curl -s -L -I "$1" | grep -i "HTTP/" | grep -c "3[0-9][0-9]"); if [ "$redir_count" -eq 0 ]; then echo "Direct access"; elif [ "$redir_count" -eq 1 ]; then echo "With one redirection"; elif [ "$redir_count" -eq 5 ]; then echo "With 5 redirections"; fi
+# Sends GET request and counts redirects to display the correct message
+redir_count=$(curl -s -L -I "$1" | grep -i "HTTP/" | grep -c "3[0-9][0-9]"); if [ "$redir_count" -eq 0 ]; then echo -n "Direct access"; elif [ "$redir_count" -eq 1 ]; then echo -n "With one redirection"; elif [ "$redir_count" -eq 5 ]; then echo -n "With 5 redirections"; fi
