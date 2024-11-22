507b0ed6dac2282c2884ed03670e26e1a3fc6564
Keane <g.keane@alustudent.com>
2024-11-22 05:29:09 -0800

    changes

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request, and displays the size of the response body in bytes.
-
-# Check if the URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a GET request using curl and display the size of the response body in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# This script takes a URL, sends a request to it, and displays the size of the response body in bytes.
+curl -s "$1" | wc -c
